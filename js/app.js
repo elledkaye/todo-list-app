cdeeae4aba25fc87c6718ef751af5c99abb2336a
Danielle Kaye <41302248+elledkaye@users.noreply.github.com>
2019-12-23 15:07:28 -0500

    Added JS

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
+//SELECT THE ELEMENTS//
 var clear = document.querySelector(".clear");
 const dateElement = document.getElementById("date");
 const list = document.getElementById("list");  
 var input = document.getElementById ("input"); 
 
 
-//CLASSES NAMES// 
+//CLASS NAMES// 
 const CHECK = "fa fa-check-circle";
 const UNCHECK  = "fa fa-circle-thin";
 const LINE_THROUGH = "linethrough";
@@ -16,13 +17,30 @@ dateElement.innerHTML = today.toLocaleDateString("en-US", options);
 
 
 //ADD TODO//
-function addToDO(toDo){ 
-    const item = '<li class = "item"> <i class = "fa fa-circle-thin co" job ="complete" id ="0" ></i> <p class = "text">${toDo}</p> </li>'
-    list.insertAdjacentHTML(position, item);
+function addToDo(toDo){ 
+    const position = "beforeend";
+    const item = `<li class = "item"> 
+     <i class = "fa fa-circle-thin co" job ="complete">
+     id ="0"></i> 
+     <p class = "text">${toDo}</p> 
+     </li>`
+    list.insertAdjacentHTML(position, item); 
 
 } 
 
-//CLEAR TODO// 
-function clearToDO(){ 
+//ADD AN ITEM// 
+//to the list using the enter key//
+document.addEventListener("keyup",function(event){ 
+    if(event.keyCode == 13){ 
+        const toDo = input.value;
+        //if the input value is not empty add todo//
+        if(toDo){ 
+            addToDo(toDo);
+        } 
+input.value = " ";
+    }
     
-}
+});
+
+//CLEAR TODO// 
+
